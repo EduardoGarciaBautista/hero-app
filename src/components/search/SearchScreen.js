3eacaec2407b3002c9cfb809d0a6643420c4241d
eduardo <egarciab@ids.com.mx>
2020-10-01 00:07:02 -0500

    [ADD] Style responsive to search component

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -29,53 +29,54 @@ export const SearchScreen = ({history}) => {
         <div>
             <h1>Search Screen</h1>
             <hr/>
-
-            <div className="row">
-                <div className="col-5">
-                    <h4>Search Form</h4>
-                    <form onSubmit={handleSearch}>
-                        <input
-                            type="text"
-                            placeholder="Find Hero"
-                            className="form-control"
-                            name="query"
-                            value={query}
-                            onChange={handleInputChange}
-                            autoComplete="off"
-                        />
-                        <button
-                            type="submit"
-                            className="btn btn-block btn-outline-primary"
-                        >
-                            Search...
-                        </button>
-                    </form>
-                </div>
-                <div className="col-7">
-                    <h4>Results</h4>
-                    <hr/>
-                    {q === '' &&
-                    <div className="alert alert-info text-center">
-                        Search a hero
-                    </div>
-                    }
-
-                    {(q !== '' && heroesFiltered.length === 0) &&
-                    <div className="alert alert-danger text-center">
-                        There's no hero with text {q}
-                    </div>
-                    }
-
-                    {heroesFiltered.map(hero => {
-                        return (
-                            <HeroCard
-                                key={hero.id}
-                                {...hero}
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-5">
+                        <h4>Search Form</h4>
+                        <form onSubmit={handleSearch}>
+                            <input
+                                type="text"
+                                placeholder="Find Hero"
+                                className="form-control"
+                                name="query"
+                                value={query}
+                                onChange={handleInputChange}
+                                autoComplete="off"
                             />
-                        )
-                    })
+                            <button
+                                type="submit"
+                                className="btn btn-block btn-outline-primary"
+                            >
+                                Search...
+                            </button>
+                        </form>
+                    </div>
+                    <div className="col-sm-7">
+                        <h4>Results</h4>
+                        <hr/>
+                        {q === '' &&
+                        <div className="alert alert-info text-center">
+                            Search a hero
+                        </div>
+                        }
 
-                    }
+                        {(q !== '' && heroesFiltered.length === 0) &&
+                        <div className="alert alert-danger text-center">
+                            There's no hero with text {q}
+                        </div>
+                        }
+
+                        {heroesFiltered.map(hero => {
+                            return (
+                                <HeroCard
+                                    key={hero.id}
+                                    {...hero}
+                                />
+                            )
+                        })
+
+                        }
+                    </div>
                 </div>
             </div>
         </div>
